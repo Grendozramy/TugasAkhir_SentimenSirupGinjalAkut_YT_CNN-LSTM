a2c1dab6341e054c3b966254cee5b56dafa172b9
Guido Tamara <zramyg@gmail.com>
2023-04-07 05:29:23 +0700

    v1

---
+++ Notebooks/1. Crawling - Scraping/copy yt_public.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
+from iteration_utilities import unique_everseen
+
+from utils.youtube_comments import process_comments, make_csv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+youtube = build("youtube", "v3", developerKey=API_KEY)
+
+def comment_threads(videoId, to_csv=True):
+    comments_list = []
+
+    # Fetch comments using YouTube API
+    request = youtube.commentThreads().list(
+        part='id,replies,snippet',
+        videoId=videoId,
+    )
+    response = request.execute()
+    comments_list.extend(process_comments(response['items']))
+
+    # Continue fetching comments if nextPageToken exists
+    while response.get('nextPageToken', None):
+        request = youtube.commentThreads().list(
+            part='id,replies,snippet',
+            videoId=videoId,
+            pageToken=response['nextPageToken']
+        )
+        response = request.execute()
+        comments_list.extend(process_comments(response['items']))
+
+    # Remove duplicates from comments_list
+    comments_list = list(unique_everseen(comments_list))
+
+    print(f"Finished fetching comments for {videoId}. {len(comments_list)} comments found.")
+
+    # Save comments to CSV file if to_csv is True
+    if to_csv:
+        make_csv(comments_list, videoId)
+
+    return comments_list
+
+if __name__ == '__main__':
+    videoId = 'IAQrhMowzZY'
+    response = comment_threads(videoId)
+    print(response)
--- Notebooks/1. Crawling - Scraping/yt_public.py
+++ Notebooks/1. Crawling - Scraping/yt_public.py
@@ -3,47 +3,47 @@ from dotenv import load_dotenv
 from googleapiclient.discovery import build
 from iteration_utilities import unique_everseen
 
-from utils.comments import process_comments, make_csv
+# Import the modified process_comments and make_csv functions
+from utils.youtube_comments import process_comments, make_csv
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 youtube = build("youtube", "v3", developerKey=API_KEY)
 
-def comment_threads(channelID, to_csv=True):
-    
+def comment_threads(videoId, to_csv=True):
     comments_list = []
-    
+
+    # Fetch comments using YouTube API
     request = youtube.commentThreads().list(
-        part='id,replies,snippet',
-        videoId=channelID,
+        part='id,snippet',
+        videoId=videoId,
     )
     response = request.execute()
-    comments_list.extend(process_comments(response['items']))
+    comments_list.extend(process_comments(response['items'], csv_output=False))
 
-    # if there is nextPageToken, then keep calling the API
+    # Continue fetching comments if nextPageToken exists
     while response.get('nextPageToken', None):
         request = youtube.commentThreads().list(
-            part='id,replies,snippet',
-            videoId=channelID,
+            part='id,snippet',
+            videoId=videoId,
             pageToken=response['nextPageToken']
         )
         response = request.execute()
         comments_list.extend(process_comments(response['items']))
 
+    # Remove duplicates from comments_list
     comments_list = list(unique_everseen(comments_list))
 
-    print(f"Finished fetching comments for {channelID}. {len(comments_list)} comments found.")
-    
+    print(f"Finished fetching comments for {videoId}. {len(comments_list)} comments found.")
+
+    # Save comments to CSV file if to_csv is True
     if to_csv:
-        make_csv(comments_list, channelID)
-    
+        make_csv(comments_list, videoId)
+
     return comments_list
 
-
 if __name__ == '__main__':
-    channelId = 'yiffzzl7EFY'
-
-    response = comment_threads(channelId)
-
+    videoId = 'yiffzzl7EFY'
+    response = comment_threads(videoId)
     print(response)
